2656c7f747d0efaf8a7fde42bda269fd49350017
spensation <spensation@gmail.com>
2018-08-26 14:24:15 -0700

    cleaned up country_spec.rb

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,34 +1,17 @@
 describe Country do
 
-	# before do 
-	# 	@country = FactoryBot.create(:country, :country_with_producers)
-	# end
+	context "validations" do
+		it { should validate_presence_of :name }
 
-
-	describe "validations" do
-		it 'is valid with a name' do
-			country = FactoryBot.build(:country)
-			expect(country).to be_valid
-			# @country.name.should_not == nil
-		end
-
-		it "is invalid without a name" do
-			country = FactoryBot.build(:country, :name => nil)
-			country.valid?
-			expect(country.errors[:name]).to include("can't be blank")
-		end
 	end
 
-	describe "associations" do
+	context "associations" do
+		it { should have_many(:appellations) }
+		it { should have_many(:producers)}
 
-		it 'has many appellations' do
-			assc = described_class.reflect_on_association(:appellations)
-			expect(assc.macro).to eq :has_many
-		 end
-
-		it 'has many producers through appellations' do
-			assc = described_class.reflect_on_association(:producers)
-			expect(assc.macro).to eq :has_many 
-	    end
 	end 
+
+	it "has a valid factory" do
+		expect(build(:country)).to be_valid
+	end
 end
